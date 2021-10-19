5aac7147dd06f777e62808658797a79b35720019
Gulmira817 <38330620+Gulmira817@users.noreply.github.com>
2021-10-19 08:00:14 +0400

    module4-task1

---
+++ project/src/mocks/reviews.ts
@@ -0,0 +1,19 @@
+import { Review } from '../types';
+export const reviews : Review[]=[
+  {
+    comment: 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+    rating: 4,
+  },
+  {
+    comment: 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+    rating: 5,
+  },
+  {
+    comment: 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+    rating: 1,
+  },
+  {
+    comment: 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+    rating: 3,
+  },
+];
--- project/src/types/index.ts
+++ project/src/types/index.ts
@@ -1,14 +1,45 @@
-export type City = {
-  id: number,
-  cityName:string,
-  images:string[],
-  isFavorite?: boolean,
-  isPremium?: boolean,
-  maxAdults: number,
-  previewImage: string,
-  price: number,
-  rating: number,
-  title: string,
-  type: string
+export type Location= {
+latitude:number
+longitude:number,
+zoom: number
+}
+
+export  type City = {
+  name:string,
+  location:Location
 };
 
+export type Review = {
+comment:string,
+rating:number
+}
+
+export type Host = {
+avatarUrl:string,
+name:string,
+id:number,
+isPro:boolean,
+}
+
+
+export type Offer = {
+  city:City,
+  bedrooms:number,
+  description:string,
+  goods:string[],
+  host:Host,
+  maxAdults: number,
+  previewImage: string,
+  images:string[],
+  location:Location,
+  id: number,
+  price: number,
+  type: string,
+  rating: number,
+  title: string,
+  isPremium?: boolean,
+  isFavorite?: boolean,
+}
+
+
+export type Offers=Offer[]
